5e43272e1a30567c3822437f72f1aa4ec66bdacd
Roberto <ruidaing@hotmail.com>
2022-03-19 18:30:54 -0500

    Creacion de componentes

---
+++ screens/CreateuserScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const CreateuserScreen = () => {
+  return (
+    <View>
+      <Text>Createuser Screen</Text>
+    </View>
+  );
+};
+
+export default CreateuserScreen;
