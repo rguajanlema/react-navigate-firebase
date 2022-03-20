53b247a81fda78e4df15703a9476aaf8a1b87354
Roberto <ruidaing@hotmail.com>
2022-03-19 19:35:08 -0500

    instalacion de firebase

---
+++ database/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC87PV-hX6yVl94lw47b9fzIA04juyKBRM",
+  authDomain: "react-native-firebase-5436d.firebaseapp.com",
+  projectId: "react-native-firebase-5436d",
+  storageBucket: "react-native-firebase-5436d.appspot.com",
+  messagingSenderId: "200386138881",
+  appId: "1:200386138881:web:13c52f033cdcd8b9a64100",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
