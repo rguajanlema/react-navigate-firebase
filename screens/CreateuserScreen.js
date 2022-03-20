2c12c99c8227999482ad8a4f7e0c094c60e0f310
Roberto <ruidaing@hotmail.com>
2022-03-19 19:16:52 -0500

    uso de state en createuserscreen

--- screens/CreateuserScreen.js
+++ screens/CreateuserScreen.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Button, TextInput, ScrollView, StyleSheet } from "react-native";
 
 const CreateuserScreen = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleChangetext = (name, value) => {
+    setState({ ...state, [name]: value });
+  };
+
   return (
     <ScrollView style={style.container}>
       <View style={style.inputGroup}>
-        <TextInput placeholder="Name User" />
+        <TextInput
+          placeholder="Name User"
+          onChangeText={(value) => handleChangetext("name", value)}
+        />
       </View>
       <View style={style.inputGroup}>
-        <TextInput placeholder="Email User" />
+        <TextInput
+          placeholder="Email User"
+          onChangeText={(value) => handleChangetext("email", value)}
+        />
       </View>
       <View style={style.inputGroup}>
-        <TextInput placeholder="Phone User" />
+        <TextInput
+          placeholder="Phone User"
+          onChangeText={(value) => handleChangetext("phone", value)}
+        />
       </View>
       <View>
-        <Button title="Save User" />
+        <Button title="Save User" onPress={() => console.log(state)} />
       </View>
     </ScrollView>
   );
